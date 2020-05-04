956d3b674e9d3957ee37d81405951d4021239779
Ree_ <ree.dev.jp@gmail.com>
2020-05-04 14:37:20 +0900

    add: build file

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# GIthub-artifacts-url
+# Github-artifacts-url
+[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)
+[![jitPack](https://jitpack.io/v/Ree-jp/GIthub-artifacts-url.svg)](https://jitpack.io/#Ree-jp/GIthub-artifacts-url)
+  
 get github actions latest artifact url
+
+# How to use
+```Kotlin
+val url = ArtifactUrl("https://api.github.com/repos/${owner}/${repo}").get(user, token)
+```
---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    java
+    kotlin("jvm") version "1.3.72"
+    id("com.github.johnrengelman.shadow") version "5.2.0"
+}
+
+group = "net.ree_jp"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("com.google.code.gson:gson:2.8.6")
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
